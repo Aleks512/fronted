3c4884aafea43d90d696ca2ee691d686e1a150e5
Aleks512 <aleksandracichocka999@gmail.com>
2024-06-20 20:46:25 +0200

    local url

--- src/axios-api.js
+++ src/axios-api.js
@@ -3,7 +3,7 @@ import { store } from './store';
 import router from '@/router';
 
 const getAPI = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.VUE_APP_API_URL || 'http://127.0.0.1:8000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
